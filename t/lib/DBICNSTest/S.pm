d8e1b028722b4cdc0b1d5e7ff8dba869425b92d0
Peter Rabbitson <ribasushi@cpan.org>
2009-05-19 14:22:05 +0000

    Get abbraxa's stuff to work

--- t/lib/DBICNSTest/S.pm
+++ t/lib/DBICNSTest/S.pm
@@ -1,6 +1,9 @@
 package DBICNSTest::S;
 use base qw/DBICNSTest::Result::A/;
 
+# init the resultsource instance and point it to the same table as the parent class
+__PACKAGE__->table (__PACKAGE__->result_source_instance->name); 
+
 sub submethod {
     return 'this is a new method in this subclass';
 }

c3796a220ce7f15eee69e0a3c54aea9c36e13d2d
Peter Rabbitson <ribasushi@cpan.org>
2009-09-12 10:11:41 +0000

    Reshape initial tests

--- t/lib/DBICTest/Schema/Track.pm
+++ t/lib/DBICTest/Schema/Track.pm
@@ -50,4 +50,16 @@ __PACKAGE__->belongs_to( disc => 'DBICTest::Schema::CD' => 'cd');
 __PACKAGE__->might_have( cd_single => 'DBICTest::Schema::CD', 'single_track' );
 __PACKAGE__->might_have( lyrics => 'DBICTest::Schema::Lyrics', 'track_id' );
 
+__PACKAGE__->might_have (
+    'next_track',
+    __PACKAGE__,
+    sub {
+        my ( $self_alias, $rel_alias, $self_rsrc, $rel_name ) = @_;
+        return {
+            "${self_alias}.cd" => \ "${rel_alias}.cd",
+            "${self_alias}.position" => { '<', \ "${rel_alias}.position" },
+        };
+    },
+);
+
 1;

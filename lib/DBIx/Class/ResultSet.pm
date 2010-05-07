f0d9d128e7e20aba42782ed87d3920dd23206907
Arthur Axel "fREW" Schmidt <frioux@gmail.com>
2010-05-07 02:27:18 +0000

    no filter and inflate column

--- lib/DBIx/Class/ResultSet.pm
+++ lib/DBIx/Class/ResultSet.pm
@@ -2809,17 +2809,6 @@ sub _resolved_attrs {
       @cols = @{ delete $attrs->{columns}}
     } elsif ( defined $attrs->{columns} ) {
       @cols = delete $attrs->{columns}
-    } elsif ( defined $attrs->{'remove-columns'}) {
-      for ($source->columns) {
-        # cribbed from http://www.stonehenge.com/merlyn/UnixReview/col11.html
-        # Set subtraction
-        my %temp = ();
-        @temp{$source->columns} = ();
-        foreach (@{ $attrs->{'remove-columns'} }) {
-           delete $temp{$_};
-        }
-        @cols = keys %temp;
-      }
     } else {
       @cols = $source->columns
     }
@@ -2839,6 +2828,18 @@ sub _resolved_attrs {
     }
   }
 
+  if ( defined $attrs->{'remove-columns'}) {
+     for ($attrs->{columns}) {
+        # cribbed from http://www.stonehenge.com/merlyn/UnixReview/col11.html
+        # Set subtraction
+        my %temp = ();
+        @temp{$source->columns} = ();
+        foreach (@{ $attrs->{'remove-columns'} }) {
+           delete $temp{$_};
+        }
+        @cols = keys %temp;
+     }
+  }
   # add the additional columns on
   foreach (qw{include_columns +columns}) {
     if ( $attrs->{$_} ) {

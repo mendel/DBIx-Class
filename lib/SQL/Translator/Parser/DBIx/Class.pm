dfaf29cbac09956da9835b2ce3979fc395f16078
Amiri Barksdale <amiribarksdale@gmail.com>
2010-05-28 15:06:57 +0000

    New parser sort functions fulfills its purpose but appears to make deploy_depends_on superfluous. I will merge the depeendencies and deploy_depends_on hashes to make sure any uncaught are folded in.

--- lib/SQL/Translator/Parser/DBIx/Class.pm
+++ lib/SQL/Translator/Parser/DBIx/Class.pm
@@ -11,7 +11,7 @@ use warnings;
 use vars qw($DEBUG $VERSION @EXPORT_OK);
 $VERSION = '1.10';
 $DEBUG = 0 unless defined $DEBUG;
-use Data::Dumper;
+
 use Exporter;
 use SQL::Translator::Utils qw(debug normalize_name);
 use Carp::Clan qw/^SQL::Translator|^DBIx::Class/;
@@ -292,39 +292,17 @@ EOW
     }
 
     my %views;
-    #my @view_sources =
-      #sort {
-        #(exists $a->deploy_depends_on->{$b->source_name} ? 1 : 0)
-        #<=>
-        #(exists $b->deploy_depends_on->{$a->source_name} ? 1 : 0)
-      #}
-      #map { $dbicschema->source($_) } (sort keys %view_monikers);
-
-    #my @view_sources =
-        #grep { $_->can('view_definition') } # make sure it's a view
-        #map    { $dbicschema->source($_) } # have to get a source
-        #map    { $tables{$_}{source}{source_name} } # have to get a sourcename
-        #sort {
-            #keys %{ $dependencies->{$a} || {} }
-            #<=>
-            #keys %{ $dependencies->{$b} || {} }
-            #||
-            #$a cmp $b
-        #}
-        #keys %$dependencies;
-    
+        
     my @view_sources =
-        sort {
-            keys %{ $dependencies->{$a} || {} }
-            <=>
-            keys %{ $dependencies->{$b} || {} }
-            ||
-            $a cmp $b
-        }
-        map { $dbicschema->source($_) } (sort keys %view_monikers);
-
-        print STDERR Dumper @view_sources;
-        print STDERR Dumper "Dependencies: ", $dependencies;
+    sort {
+        keys %{ $dependencies->{$a} || {} }
+        <=>
+        keys %{ $dependencies->{$b} || {} }
+        ||
+        $a cmp $b
+    }
+    map { $dbicschema->source($_) }
+    keys %view_monikers;
 
     foreach my $source (@view_sources)
     {

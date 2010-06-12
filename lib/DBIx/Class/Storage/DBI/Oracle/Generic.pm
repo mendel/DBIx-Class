8e09865eb2b96589d3bcd87fbfeb1c90e0eaf676
Rafael Kitover <rkitover@cpan.org>
2010-06-12 09:06:04 -0400

    fix matching quoted sequence, and skip count

--- lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
+++ lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
@@ -146,7 +146,7 @@ sub _dbh_get_autoinc_seq {
   $sth->execute (@bind);
 
   while (my ($insert_trigger, $schema) = $sth->fetchrow_array) {
-    my ($seq_name) = $insert_trigger =~ m!("?[.\w"]+?"?)\.nextval!i;
+    my ($seq_name) = $insert_trigger =~ m!("?[.\w"]+"?)\.nextval!i;
 
     next unless $seq_name;
 
